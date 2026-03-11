322bcfd0a567bbd30d086596e42b91242f227c76
Yakov Shevcov <yakov.shevcov@byteminds.co.uk>
2026-03-11 20:36:28 +0100

    Added link to home page, leading to users-list

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,3 +1,14 @@
+import { Link } from 'react-router';
+import { routes } from '@/shared/routes/routes';
+import styles from './home.module.scss';
+
 export const Home = () => {
-  return <h1>Hello GabaHR!👋</h1>;
+  return (
+    <div className={styles.page}>
+      <h1 className={styles.title}>Hello GabaHR! 👋</h1>
+      <Link to={routes.users.getLink()} className={styles.link}>
+        View Users
+      </Link>
+    </div>
+  );
 };
